97d447e427f53ffc3bff080954532e2b83a97f82
Maha <mahaAlsaggafx@gmail.con>
2025-08-25 15:18:47 +0300

    Clean up the CMS app module

--- apps/cms/src/app.module.ts
+++ apps/cms/src/app.module.ts
@@ -1,31 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppConfigModule } from '@common/app-config.module';
-import { Series } from '@db/entities/Series';
-import { Episode } from '@db/entities/Episode';
-import { Chapter } from '@db/entities/Chapter';
 import { SeriesModule } from './modules/series/series.module';
 import { EpisodesModule } from './modules/episodes/episodes.module';
+import { ChaptersModule } from './modules/chapters/chapters.module';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import databaseConfig from '@common/database.config';
+
 
 @Module({
   imports: [
     AppConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: +(process.env.DB_PORT || 5432),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        ssl: (String(process.env.DB_SSL).toLowerCase() === 'true') ? { rejectUnauthorized: false } : false,
-        entities: [Series, Episode, Chapter],
-        autoLoadEntities: true,
-        synchronize: false,
-      }),
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get('database');
+        if (!dbConfig) {
+          throw new Error('Database configuration not found');
+        }
+        return dbConfig;
+      },
     }),
     SeriesModule,
     EpisodesModule,
+    ChaptersModule
   ],
 })
 export class AppModule {}
