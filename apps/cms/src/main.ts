e7ee93917de174fd1af397981a55b621e8f83a4f
Maha <mahaAlsaggafx@gmail.con>
2025-08-25 12:13:01 +0300

    Update the project packages

--- apps/cms/src/main.ts
+++ apps/cms/src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import helmet from 'fastify-helmet';
-import compress from 'fastify-compress';
 import { AppModule } from './app.module';
 import { setupSwagger } from '@common/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
-  app.register(helmet as any);
-  app.register(compress as any);
   setupSwagger(app, 'CMS');
   const port = +(process.env.CMS_PORT || 3001);
   await app.listen(port, '0.0.0.0');
